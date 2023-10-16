0d016e3859dbd894825658bf9605a59f77645b8f
paterson3c <miguel.paterson@estudiante.uam.es>
2023-10-16 15:46:56 +0200

    hasta el 2B

---
+++ p1132B.py
@@ -0,0 +1,23 @@
+import numpy as np
+import p1132A as heap
+from typing import Tuple
+
+def pq_ini() -> np.ndarray:
+    return np.array([])
+
+def pq_insert(h: np.ndarray, k: int) -> np.ndarray:
+    return heap.insert_min_heap(h, k)
+
+def pq_remove(h: np.ndarray) -> Tuple[int, np.ndarray]:
+    if len(h) == 0:
+        return None, h
+    #el primer elemento es siempre el de prioridad menor
+    min_element = h[0]
+    #lo intercambiamos con el último elemento
+    h[0] = h[-1]
+    #eliminamos el último elemento
+    h = h[:-1]
+    #reordenamos el array
+    heap.min_heapify(h, 0)
+    
+    return min_element, h
